33745b1993edbde268836e4457eb649a101b224d
Remix678 <titohernandez1112011@live.com>
2020-02-20 02:32:18 -0800

    Create Untitled1.cpp

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	double y, z, x;
+	x = 0;
+	
+	if ( x > 100)
+		{
+			y = 20;
+			z = 40;
+			cout << "x is greater than 100";
+		}
+	
+	
+	
+}
+
