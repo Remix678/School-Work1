98399036029bb7198b1e20001c5be21f74e96885
Remix678 <titohernandez1112011@live.com>
2020-02-20 02:32:21 -0800

    Create Untitled1wdw.cpp

---
+++ Untitled1wdw.cpp
@@ -0,0 +1,12 @@
+
+Case 1: cout<<"You selected 1";
+break;
+Case 2: cout<<"You selected 2";
+break;
+Case 3: cout<<"You selected 3";
+break;
+Case 4: cout<<"you selected 4";
+break;
+default: cout<<"not good with number, eh";
+
+
